348fd020ec8466244ae8c34d54d078cc41cf6c13
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-27 23:50:16 -0300

    Criando componente reutilizável

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,3 +1,31 @@
+import Link from 'next/link'
+import { Image, Button } from '@chakra-ui/core'
+import styles from './header.module.css'
+
+const routes = [
+  { path: "/", label: "Início" },
+  { path: "/sobre", label: "Sobre" }
+]
+
 export default function Header() {
-  return <h1>Cabeçalho</h1>
+  return (
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <div>
+          <Image size="64px" src="https://via.placeholder.com/64" alt="Foto de perfil"/>
+        </div>
+        <nav>
+          <ul>
+            {routes.map(({ path, label }, idx) => (
+              <li key={idx}>
+                <Link href={path}>
+                  <Button>{label}</Button>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
