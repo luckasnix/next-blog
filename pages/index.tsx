e7f451de010356a382fa7876d40ed738a62eac96
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-12-04 18:24:40 -0300

    Exibindo a lista de postagens

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next'
-import { getSortedPostsData } from '../lib/posts'
+import { getSortedPostsData, PostFrontMatter } from '../lib/posts'
+import { Heading } from '@chakra-ui/core'
+import styles from '../styles/home.module.scss'
 
 export const getStaticProps: GetStaticProps = async () => {
   const sortedPostsData = getSortedPostsData()
@@ -10,10 +12,24 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
-export default function Home() {
+export interface HomeProps {
+  sortedPostsData: PostFrontMatter[]
+}
+
+export default function Home({ sortedPostsData }: HomeProps) {
   return (
-    <>
-      <h1>Bem-vindo!</h1>
-    </>
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <Heading as="h1">Todas as postagens</Heading>
+        <ul>
+          {sortedPostsData.map(({ slug, title, date }) => (
+            <li key={slug}>
+              <Heading as="h2">{title}</Heading>
+              <time>{date}</time>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
