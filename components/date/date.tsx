7071c3bd76b4065ad5dcbaf603a3057c2e6d9110
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-12-13 21:47:09 -0300

    Tipando o componente de data

--- components/date/date.tsx
+++ components/date/date.tsx
@@ -1,7 +1,11 @@
 import { parseISO, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
-export default function Date({ dateString }) {
+export interface DateProps {
+  dateString: string
+}
+
+export default function Date({ dateString }: DateProps) {
   const date = parseISO(dateString)
   return (
     <time dateTime={dateString}>
