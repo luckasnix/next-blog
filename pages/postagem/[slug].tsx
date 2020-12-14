050624480e4a9ed288aa94eaa71080bdcb6a96df
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-12-13 22:51:10 -0300

    Buscando o slug na rota

--- pages/postagem/[slug].tsx
+++ pages/postagem/[slug].tsx
@@ -1,5 +1,20 @@
-export default function Post() {
+import { GetServerSideProps } from 'next'
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  let { slug } = ctx.params
+  if (slug instanceof Array) {
+    slug = slug[0]
+  }
+  
+  return {
+    props: {
+      slug
+    }
+  }
+}
+
+export default function Post({ slug }) {
   return (
-    <h1>Aqui deve vir a postagem</h1>
+    <h1>O slug é: {slug}</h1>
   )
 }
